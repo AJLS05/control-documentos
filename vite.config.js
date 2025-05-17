83323c49ea71e2c36bc58e31b36393c1a44806bd
Jorkaeff Lainez <33355533+AJLS05@users.noreply.github.com>
2025-05-16 23:36:38 -0600

    Mover iconos 3

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,12 @@ export default defineConfig({
         theme_color: '#1e40af',
         icons: [
           {
-            src: '/control-tramites/icons/icon-192x192.png',
+            src: '/control-documentos/src/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/control-tramites/icons/icon-512x512.png',
+            src: '/control-documentos/src/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
